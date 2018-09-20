e9188bd172886b7a3bf440d048261359c35e7e8b
Jorn Verhoeven <bornojj@hotmail.com>
2018-09-20 12:31:04 +0200

    correct model for entity

--- XillioAPIService/Model/Entity.cs
+++ XillioAPIService/Model/Entity.cs
@@ -1,12 +1,23 @@
-﻿namespace XillioAPIService.Model
-{
-    class Entity
-    {
-        string id;
-        string kind;
-        class Original
-        {
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
+namespace XillioAPIService.Model
+{
+    public class Entity
+    {
+        public string ID { get; set; }
+        public string Kind { get; set; }
+        public XDIP Xdip { get; set; }
+        
+        public List<JObject> Original { get; set; }
+        public List<JObject> Modified { get; set; }
+
+        public Entity()
+        {
         }
+    }
+
+    public class XDIP
+    {
     }
 }
